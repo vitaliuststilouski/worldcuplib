db0cf00a1a64f934ba204da999f5baa00e788e49
Vitali.Ustsilouski <v.ustilovsky@itransition.com>
2025-07-01 16:39:05 +0200

    add tests covering empty string, undefined, null

--- __tests__/team.test.ts
+++ __tests__/team.test.ts
@@ -6,4 +6,22 @@ describe("Team", () => {
     expect(name).toBe("Brazil");
     expect(score).toBe(0);
   });
+
+  it("should throw if name is empty", () => {
+    expect(() => new Team("")).toThrow("Team name must be a non-empty string");
+  });
+
+  it("should throw an error if name is onyl whitespace", () => {
+    expect(() => new Team("   ")).toThrow(
+      "Team name must be a non-empty string"
+    );
+  });
+  it("should throw if name is null or undefined", () => {
+    expect(() => new Team(null as unknown as string)).toThrow(
+      "Team name must be a non-empty string"
+    );
+    expect(() => new Team(undefined as unknown as string)).toThrow(
+      "Team name must be a non-empty string"
+    );
+  });
 });
